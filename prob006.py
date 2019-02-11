7901deb73693fc2a26a3e49fa3178aa747cc8468
Mohit Khandelwal <mohitkh7@gmail.com>
2019-02-11 18:02:01 +0530

    optimize prob 6 solution

--- prob006.py
+++ prob006.py
@@ -13,14 +13,12 @@ and the square of the sum is 3025 − 385 = 2640.
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
-sum_of_square = 0
-sum_of_number = 0
+# using property = (1+2+3+...+n)^2 = ((n(n+1))^2)/4
+# and property = (1^2 + 2^2 + ....+ n^2) = n*(n+1)*(2n+1)*(1/6)
+n = 100
 
-for n in range(1, 101):
-    sum_of_square += n**2
-    sum_of_number += n
-
-square_of_sum = sum_of_number**2
+sum_of_square = (n * (n + 1) * ((2 * n) + 1)) // 6
+square_of_sum = ((n * (n + 1))**2) // 4
 
 # Difference
 print(square_of_sum - sum_of_square)
